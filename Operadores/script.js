2d0382fdc6b487f520aded64a303d85dd04567ce
Davi Fernandes <davi.fer159@gmail.com>
2024-12-02 17:29:37 -0300

    operadores aritimeticos

---
+++ Operadores/script.js
@@ -0,0 +1,28 @@
+// Operadores com Javascript
+const x = 5;
+const y = 4;
+
+// Operadores Aritiméticos
+const soma = x + y;
+const subtracao = x - y;
+const multiplicacao = x * y;
+const divisao = x / y;
+const restoDeDivisao = x % y;
+
+// Operadores de Incremento e Decremento
+let z = 1;
+z = z + 1;
+z += 1;
+z -= 1;
+z++;
+z--;
+
+let c = 10;
+c *= 10;
+c /= 10;
+c %= 10;
+
+
+
+
+
