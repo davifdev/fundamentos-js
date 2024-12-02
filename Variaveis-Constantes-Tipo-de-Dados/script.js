13f2f2d99e695ba689dddc20459fb8799aa2a13b
Davi Fernandes <davi.fer159@gmail.com>
2024-12-02 16:08:25 -0300

    variaveis e tipos de dados

---
+++ Variaveis-Constantes-Tipo-de-Dados/script.js
@@ -0,0 +1,8 @@
+// Váriaveis, Constantes e Tipos de Dados
+const n1 = 10; // number
+const n2 = 3.5; // number
+const s1 = "Davi"; // string
+const s2 = "Fernandes"; // string
+
+const verdadeiro = true; // boolean
+const falso = falso; // boolean
