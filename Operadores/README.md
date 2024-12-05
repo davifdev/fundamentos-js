ad609e7ec85c60636f7f219b4377545367f7d55b
Davi Fernandes <davi.fer159@gmail.com>
2024-12-05 11:52:44 -0300

    Update README.md

---
+++ Operadores/README.md
@@ -0,0 +1,63 @@
+# Operadores
+
+## Operadores aritiméticos
+
+- + : Adição;
+- - : Subtração;
+- * : Multiplicação;
+- / : Divisão;
+- % : Módulo / Resto da Divisão;
+
+## Operadores de atribuição
+
+- = : operador de atribuição
+- += ;
+- -= ;
+- *= ;
+- /= ;
+
+## Operadores de incremento e decremento
+
+- ++ ;
+- -- ;
+
+## Operadores de Comparação
+
+- Maior do que > ;
+- Menor do que < ;
+- Maior ou igual a >= ;
+- Menor ou igual a <=;
+- Igual == ;
+- Diferente !=;
+
+## Operadores Lógicos
+
+- && : and;
+- || : or;
+- ! : not;
+
+Operador &&:
+
+true && true = true;
+
+true && false = false;
+
+false && true = false;
+
+false && false = false;
+
+Operador ||;
+
+true && true = true;
+
+true || false = true;
+
+false || true = true;
+
+false || false = false;
+
+Operador Not;
+
+!true = false;
+
+!false = true;
--- Operadores/script.js
+++ Operadores/script.js
@@ -9,13 +9,17 @@ const multiplicacao = x * y;
 const divisao = x / y;
 const restoDeDivisao = x % y;
 
-// Operadores de Incremento e Decremento
+// Operadores de atribuição
 let z = 1;
 z = z + 1;
 z += 1;
 z -= 1;
+
+// Operadores de Incremento e Decremento
 z++;
 z--;
+--z;
+++z;
 
 let c = 10;
 c *= 10;
@@ -26,9 +30,27 @@ c %= 10;
 const a = 1;
 const b = 2;
 
-console.log(a > b); // false
-console.log(a < b); // true
-console.log(a >= b); // false
-console.log(a <= b); // true
-console.log(a == b); // false
-console.log(a != b); // true
+// console.log(a > b); // false
+// console.log(a < b); // true
+// console.log(a >= b); // false
+// console.log(a <= b); // true
+// console.log(a == b); // false
+// console.log(a != b); // true
+
+// Operadores Lógicos AND, OR, NOT
+
+// Operador AND
+console.log(a < b && a != b); // true -> true;
+console.log(a != b && a > b); // true && false -> false;
+console.log(a == b && a < b); // false && true -> false;
+console.log(a > b && a == b); // fasle && false -> false;
+
+// Operador OR
+console.log(a < b || a != b); // true -> true
+console.log(a != b || a > b); // true || false -> true;
+console.log(a == b || a < b); // false || true -> true;
+console.log(a == b || a > b); // false || false -> false; false
+
+// Operador Not
+console.log(!(a == b)); // false -> true;
+console.log(!(a < b)); // true -> false;
