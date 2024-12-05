b055bb56f32f1d452099d4e563df6bc6d2817c7e
Davi Fernandes <davi.fer159@gmail.com>
2024-12-05 12:14:36 -0300

    strings javascript

---
+++ Strings/script.js
@@ -0,0 +1,12 @@
+// Strings
+const nome = "Davi";
+const sobrenome = "Fernandes";
+const idade = 24;
+
+// Concatenação
+console.log(
+  "Menu nome é " + nome + " " + sobrenome + " e eu tenho " + idade + " anos."
+);
+
+// Template de Strings
+console.log(`Meu nome é ${nome} ${sobrenome} e eu tenho ${idade} anos.`);
